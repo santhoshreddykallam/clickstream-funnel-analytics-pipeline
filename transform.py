5a48ba7d8a9c610a6196b32420282f3c2179a100
santhoshreddykallam <kallamsanthoshreddy@gmail.com>
2026-07-28 16:27:29 +0530

    feat: transform - add funnel_stage column using when/otherwise on event_type

--- transform.py
+++ transform.py
@@ -2,12 +2,27 @@ import os
 os.environ["PYSPARK_PYTHON"] = r"C:\Users\hp\AppData\Local\Programs\Python\Python310\python.exe"
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import when, col
 spark = SparkSession.builder.appName('ClickstreamTransform').getOrCreate()
 
 df_pyspark = spark.read.csv('2019-Oct.csv', header = True, inferSchema = True)
 
 df_pyspark = df_pyspark.drop('category_code')
 df_pyspark = df_pyspark.na.drop(how = 'any', subset = ['user_session'])
+
+df_pyspark = df_pyspark.withColumn(
+    "funnel_stage",
+    when(col("event_type") == "view", 1)
+    .when(col("event_type") == "cart", 2)
+    .when(col("event_type") == "remove_from_cart", 3)
+    .otherwise(4)
+)
+
+df_pyspark.select(
+    "event_type",
+    "funnel_stage",
+    "user_session"
+).show(10, truncate = False)
 
 df_pyspark.printSchema()
 
